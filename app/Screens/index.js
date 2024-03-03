0f087aee24c2171f4194d44e302f2c216dee3eab
MABA1001 <mughalabdullah2905@gmail.com>
2024-03-03 22:06:27 +0500

    getting live fixture id and loc the screen

--- app/Screens/index.js
+++ app/Screens/index.js
@@ -5,12 +5,18 @@ import { useEffect, useState, useRef } from 'react';
 import { Video, ResizeMode } from 'expo-av';
 import Header from '../../Components/Header';
 import PlayerCard from './PlayerCard';
+import axios from 'axios';
 
 export default function App() {
   const [orientation, setOrientation] = useState(1);
+  const [isLocked, setIsLocked] = useState(false);
   const video = useRef(null);
+  const [timezoneData, setTimezoneData] = useState(null);
+  const [fixtureId, setFixtureId] = useState(null);
+
   useEffect(() => {
     lockOrientation();
+    fetchTimezoneData();
   }, []);
 
   const lockOrientation = async () => {
@@ -19,6 +25,42 @@ export default function App() {
     );
     const o = await ScreenOrientation.getOrientationAsync();
     setOrientation(o);
+  };
+
+  const toggleLock = () => {
+    setIsLocked(!isLocked);
+  };
+
+  const fetchTimezoneData = async () => {
+    try {
+      const options = {
+        method: 'GET',
+        url: 'https://api-football-v1.p.rapidapi.com/v3/fixtures',
+        params: {
+          season: '2023',
+          team: '33',
+          last: '1'
+        },
+        headers: {
+          'X-RapidAPI-Key':
+            'e0f48d2c0cmsha8e774e7a2187dep1ed397jsn8c42560af15d',
+          'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+        }
+      };
+
+      const response = await axios.request(options);
+      setTimezoneData(response.data);
+      console.log(response.data);
+
+      // Save the fixture ID to state
+      if (response.data.response && response.data.response.length > 0) {
+        const fixtureId = response.data.response[0].fixture.id;
+        setFixtureId(fixtureId);
+        console.log(response.data.response[0].fixture.id);
+      }
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
@@ -33,27 +75,44 @@ export default function App() {
           useNativeControls
           resizeMode={ResizeMode.CONTAIN}
           isLooping
+          onTouchStart={
+            isLocked
+              ? () => {
+                  video.current.pauseAsync();
+                }
+              : null
+          }
         />
-        <View style={styles.overlay}>
-          <Header />
-          <TouchableOpacity style={styles.button}>
-            <Text style={styles.buttonText}>Tap on a player</Text>
-          </TouchableOpacity>
-          <View style={styles.PlayerCardStyles}>
-            <PlayerCard
-              name="Mirza Abdullah"
-              imageSource={require('./../../assets/playerDemo.png')}
-              stats={{
-                goals: 10,
-                assists: 5,
-                shots: 50,
-                passes: 200,
-                tackles: 30
-              }}
-              onToggleSwitch={() => {}} // Placeholder for the onToggleSwitch function
-            />
+        {!isLocked && (
+          <View style={styles.overlay}>
+            <Header />
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Tap on a player</Text>
+            </TouchableOpacity>
+            <View style={styles.PlayerCardStyles}>
+              <PlayerCard
+                name="Mirza Abdullah"
+                imageSource={require('./../../assets/playerDemo.png')}
+                stats={{
+                  goals: 10,
+                  assists: 5,
+                  shots: 50,
+                  passes: 200,
+                  tackles: 30
+                }}
+                onToggleSwitch={() => {}} // Placeholder for the onToggleSwitch function
+              />
+            </View>
           </View>
-        </View>
+        )}
+        <TouchableOpacity
+          style={[styles.lockButton, { right: 10, top: 10 }]}
+          onPress={toggleLock}
+        >
+          <Text style={styles.lockButtonText}>
+            {isLocked ? 'Unlock' : 'Lock'}
+          </Text>
+        </TouchableOpacity>
       </View>
       <StatusBar style="auto" hidden={true} />
     </View>
@@ -99,5 +158,19 @@ const styles = StyleSheet.create({
     margin: 10,
     marginLeft: 40,
     borderRadius: 10
+  },
+  lockButton: {
+    position: 'absolute',
+    width: 40,
+    height: 40,
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  lockButtonText: {
+    fontSize: 16,
+    color: 'black',
+    fontWeight: 'bold'
   }
 });
